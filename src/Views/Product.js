74a7405154aeb9fb0282d7781ef1ea482730f09b
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2020-11-14 22:48:14 +0000

    error reporting & loading sign

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,31 +1,46 @@
 import React, { useEffect, useState, } from 'react';
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
+import Loader from '../Components/Loader';
 
 function Product() {
     const { id } = useParams()
     const url = `https://5fb050d17edddb00164683e7.mockapi.io/products/${id}`
     const [product, setProduct] = useState({
         loading: false,
-        data:null
+        data: null,
+        error:false
     })
     let content = null
     useEffect(() => {
         setProduct({
             loading: true,
-            data: null
+            data: null,
+            error:false
         })
         axios.get(url).then(response => {
             setProduct({
                 loading: false,
-                data: response.data
+                data: response.data,
+                error:false
             })
-    
+        }).catch(() => {
+            setProduct({
+                loading: false,
+                data: null,
+                error:true
+            })
         })
     }, [url])
 
+    if (product.error) {
+        content = <p>
+            There was an error. You may of searched for a product that doesn't exist.  Please refresh or try again later.
+        </p>
+    }
+
     if (product.loading) {
-        content = <p>loading...</p>
+        content =<Loader> </Loader>
     }
 
     if (product.data) {
